7e7b6400e187e9bd90dc9e43c21fec3e99b9dac5
Rishabh Khanna <rishabhrrk@gmail.com>
2020-07-01 19:04:27 -0400

    many changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,139 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default class Porfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
-    return (
-      <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-          {
-            resumeData.portfolio && resumeData.portfolio.map((item)=>{
-              return(
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src={`${item.imgurl}`} className="item-img"/>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>{item.name}</h5>
-                          <p>{item.description}</p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              )
-            })
-          }
+
+    /*
+    "name":"Nitrogen Rate Predictor",
+        "description":"A web portal and an android app that provides the appropriate nitrogen rate that should be used by the Maine farmers for growing potato. The prediction of nitrogen rate is based on a linear regression model",
+        "technologies":["React", "React-Native", "Nodejs", "Express", "Postgresql"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/nrate/"
+        */
+
+    const portfolioItems = resumeData.portfolio.map((portfolioItem, idx) => {
+      return (
+        <div key={idx} style={{ width: "33%", padding: "2rem" }}>
+          <div
+            style={{
+              position: "relative",
+              backgroundColor: "white",
+              padding: "2rem",
+              height: "100%",
+            }}
+          >
+            <h2
+              style={{
+                textAlign: "center",
+                fontSize: "2rem",
+                color: "#313131",
+              }}
+            >
+              {portfolioItem.name}
+            </h2>
+            <p>{portfolioItem.description}</p>
+
+            <a
+              style={{
+                position: "absolute",
+                bottom: "0%",
+                left:"0%",
+                padding: "1rem",
+              }}
+              href={portfolioItem.url}
+              target="_blank"
+            >
+              <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
+            <a
+              style={{
+                position: "absolute",
+                bottom: "0%",
+                right:"0%",
+                padding: "1rem",
+              }}
+              href={portfolioItem.youtube}
+              target="_blank"
+            >
+              <i className="fa fa-youtube-play" aria-hidden="true"></i>
+            </a>
           </div>
         </div>
-      </div>
-  </section>
-        );
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <h1>Check out some of my work</h1>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "flex-row",
+            flexWrap: "wrap",
+            padding: "1rem",
+            maxWidth: "1200px",
+            margin: "0 auto",
+          }}
+        >
+          {portfolioItems}
+        </div>
+      </section>
+    );
   }
 }
+
+// export default class Porfolio extends Component {
+//   render() {
+//     let resumeData = this.props.resumeData;
+//     return (
+//       <section id="portfolio">
+//         <div className="row">
+//           <div className="twelve columns collapsed">
+//             <h1>Check Out Some of My Works.</h1>
+//             <div
+//               id="portfolio-wrapper"
+//               className="bgrid-quarters s-bgrid-thirds cf"
+//             >
+//               {resumeData.portfolio &&
+//                 resumeData.portfolio.map((item) => {
+//                   return (
+//                     <div className="columns portfolio-item">
+//                       <div className="item-wrap">
+//                         <a href={item.url}>
+//                           {/* <img src={`${item.imgurl}`} className="item-img"/> */}
+//                           <div className="overlay">
+//                             <div className="portfolio-item-meta">
+//                               <h5>{item.name}</h5>
+//                               <p>{item.description}</p>
+//                               <ul>
+//                                 {resumeData.portfolio.technologies &&
+//                                   resumeData.portfolio.technologies.map(
+//                                     (tech) => {
+//                                       return (
+//                                         <li key={item.name}>
+//                                           <a
+//                                             href={tech.technologies}
+//                                             target="_blank"
+//                                           ></a>
+//                                         </li>
+//                                       );
+//                                     }
+//                                   )}
+//                               </ul>
+//                             </div>
+//                           </div>
+//                         </a>
+//                       </div>
+//                     </div>
+//                   );
+//                 })}
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
--- src/resumeData.js
+++ src/resumeData.js
@@ -18,16 +18,16 @@ let resumeData = {
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/smartrrk",
           "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a master's student at University of Florida majoring in Computer Sciences. I am a self taught programmer, currently diving deeper into Full-Stack development. If I were to describe my self in a sentence I would say - “I take responsibility for my craft and believe in my abilities to build a great product”.",
     "address":"Gainesville, Florida, US",
-    "website":"https://rishabhrrk.github.io",
+    "website":"https://rishabhrrk.github.io/react/",
     "education":[
       {
-        "UniversityName":"University of Floriday",
+        "UniversityName":"University of Florida",
         "specialization":"MS Computer Science",
         "GPA":"3.67/4",
         "MonthOfStarting":"August",
@@ -97,24 +97,58 @@ let resumeData = {
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Nitrogen Rate Predictor",
+        "description":"A web portal and an android app that provides the appropriate nitrogen rate that should be used by the Maine farmers for growing potato. The prediction of nitrogen rate is based on a linear regression model",
+        "technologies":["React", "React-Native", "Nodejs", "Express", "Postgresql"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://rishabhrrk.github.io/nrate/",
+        "youtube":""
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"Budiary",
+        "description":"The application keeps track of budgets and expenses.",
+        "technologies":["Nodejs", "Express", "MongoDB"],
+        "imgurl":"images/portfolio/project.jpg",
+        "url":""
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "name":"Microblogger",
+        "description":"A microblogger similar to Twitter, with features such as tweeting, re-tweeting, hash tag searching and subscribing to users was",  
+        "technologies":["Elixir", "Phoneix", "Vue.js"],
+        "imgurl":"images/portfolio/project2.png",
+        "url":"https://github.com/rishabhrrk/twitter_engine"
       },
       {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"ETL using Spark",
+        "description":"Aim of the PoC was to check the viability of Spark and compare performance of Spark and SSIS. Real-time data streaming from FTP servers was smoother in Spark. Although data processing time was less for Spark but SSIS was easier to implement",
+        "technologies":["Spark", "Spark-SQL", "SSIS"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":""
+
+      },
+      {
+        "name":"Tweet Analysis",
+        "description":"Aim of the project was to analyze election trends in the state of Rajasthan, India. Fetching of tweets was achieved via Spark Streaming and processing of data was done using Spark SQL in Scala. Data visualization was implemented using R",
+        "technologies":["Spark", "Spark-SQL", "R"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/tweet_analysis"
+
+      },
+      {
+        "name":"Gossip-Push Sum",
+        "description":"Aim of the project was to analyze election trends in the state of Rajasthan, India. Fetching of tweets was achieved via Spark Streaming and processing of data was done using Spark SQL in Scala. Data visualization was implemented using R",
+        "technologies":["Spark", "Spark-SQL", "R"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/gossip-push-sum"
+
+      },
+      {
+        "name":"Tapestry",
+        "description":"Aim of the project was to analyze election trends in the state of Rajasthan, India. Fetching of tweets was achieved via Spark Streaming and processing of data was done using Spark SQL in Scala. Data visualization was implemented using R",
+        "technologies":["Spark", "Spark-SQL", "R"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/tapestry"
+
       }
     ],
     "testimonials":[
