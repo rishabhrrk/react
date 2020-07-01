64a4df6e0ee4fbb8494c5beebfb1ee71b41087cd
Rishabh Khanna <rishabhrrk@gmail.com>
2020-07-01 10:35:10 -0400

    Updates

---
+++ precache-manifest.e2577acedc8b5893c6e037e6bd156e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2097e16adce304683052cd59418f26b9",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ad9610ca5bd8fa0c53c7",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/react/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9610ca5bd8fa0c53c7",
+    "url": "/react/static/js/main.766719a7.chunk.js"
+  },
+  {
+    "revision": "4e6b35c249221b5aa102",
+    "url": "/react/static/js/runtime-main.121b4b3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
