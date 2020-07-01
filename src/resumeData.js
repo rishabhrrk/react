766ff4a28804d3189b38a68499c0ceaa599d1151
Rishabh Khanna <rishabhrrk@gmail.com>
2020-07-01 11:21:03 -0400

    ported changes

---
+++ src/resumeData.js
@@ -0,0 +1,132 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rishabh Khanna",
+    "role": "Software Developer, Web Developer and Data Engineer",
+    "linkedinId":"rishabhrrk",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of software and web development and like to learn about new technologies, build personal projects. When I am free I like to go on beer tasting or hangout with friends",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rishabhrrk/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rishabhrrk",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a master's student at University of Florida majoring in Computer Sciences. I am a self taught programmer, currently diving deeper into Full-Stack development. If I were to describe my self in a sentence I would say - “I take responsibility for my craft and believe in my abilities to build a great product”.",
+    "address":"Gainesville, Florida, US",
+    "website":"https://rishabhrrk.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Floriday",
+        "specialization":"MS Computer Science",
+        "GPA":"3.67/4",
+        "MonthOfStarting":"August",
+        "YearOFStarting":"2019",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2020",
+        "Achievements":"Coursework – Algorithms, Advanced Data Structures, Distributed Systems, Computer Networks"
+      },
+      {
+        "UniversityName":"GGSIPU",
+        "specialization":"B.Tech. Computer Science",
+        "GPA":"73.1/100",
+        "MonthOfStarting":"August",
+        "YearOFStarting":"2012",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+        "Achievements":"Coursework – Algorithms, Operating Systems, OOP using Java, Software Engineering, DBMS, Data Warehousing & Minin"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"O9 Solutions",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "MonthOfJoining":"May",
+        "YearOfJoining":"2018",
+        "Achievements":"• Single-handedly implemented an Extract Transform Load (ETL) solution for Navico (client) using SSIS and SQL Server 2012        • Developed Python and C# scripts for interacting with various platform APIs        • Used Apache Spark and Scala to perform data cleaning and ETL as part of a self-initiated Proof of Concept (PoC)"
+      },
+      {
+        "CompanyName":"Tata Consultancy Services",
+        "specialization":"Assistant System Engineer",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2018",
+        "MonthOfJoining":"October",
+        "YearOfJoining":"2016",
+        "Achievements":"• Developed PL/SQL procedures and functions for inventory optimization of ABB (client)        • Tested and improved performance of existing PL/SQL procedures by achieving reduction in running time by 30%        • Single-handedly built a PoC by implementing the existing solution in the Hadoop ecosystem to improve performance which was selected by higher management for further development. The solution reduced running time and cost of the project        • Automated various daily tasks such as uploading a file and generating logs using batch scripting        • Used MVC framework in C#, ASP .NET and SQL Server 2008 to build a ticketing application for a customer service center        • Automated various web & Windows applications using .NET and C# as part of Robo-Process Automation team"
+      }
+    ],
+    "skillsDescription":[
+      
+        "Programming Languages : Python, C#, Java, Scala, Elixir, JavaScript"
+      ,
+      
+        "Frameworks : React, Nodejs, React Native, ASP .NET, Apache Spark, Hadoop"
+      ,
+      
+        "Databases : SQL Server, MongoDB, PostgreSQL, MySQL, Oracle"
+      ,
+      
+        "Others : SQL, T-SQL, PL/SQL, SSIS, Batch Scripting, R"
+      
+    ],
+    "skills":[
+      {
+        "Programming Languages":"Python, C#, Java, Scala, Elixir, JavaScript"
+      },
+      {
+        "Frameworks":"React, Nodejs, React Native, ASP .NET, Apache Spark, Hadoop"
+      },
+      {
+        "Databases":"SQL Server, MongoDB, PostgreSQL, MySQL, Oracle"
+      },
+      {
+        "Others":"SQL, T-SQL, PL/SQL, SSIS, Batch Scripting, R"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
