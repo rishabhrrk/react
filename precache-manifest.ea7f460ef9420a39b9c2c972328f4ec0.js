9cb76f4f7bd443b769cc9ea2050c5a501c4fe90d
Rishabh Khanna <rishabhrrk@gmail.com>
2020-07-01 20:19:33 -0400

    Updates

--- precache-manifest.ea7f460ef9420a39b9c2c972328f4ec0.js
+++ precache-manifest.ea7f460ef9420a39b9c2c972328f4ec0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4828fe6acdf57cfe791deaac6746b0d2",
+    "revision": "ee8c8171ea09a158ecb2c2f45c0d5268",
     "url": "/react/index.html"
   },
   {
-    "revision": "9d04b2eb97af350b3d9c",
+    "revision": "45d338aeb909c4807c68",
     "url": "/react/static/css/main.e8c24495.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react/static/js/2.5f03bd65.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d04b2eb97af350b3d9c",
-    "url": "/react/static/js/main.70fa233a.chunk.js"
+    "revision": "45d338aeb909c4807c68",
+    "url": "/react/static/js/main.7e3a2f42.chunk.js"
   },
   {
     "revision": "4e6b35c249221b5aa102",
